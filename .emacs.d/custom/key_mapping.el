6f4059c6a8d58a44240052294f69a230a3c52853
stillfarmer2011 <stillfarmer2011@gmail.com>
2016-06-23 08:55:07 +0800

    add goto-line key map

--- .emacs.d/custom/key_mapping.el
+++ .emacs.d/custom/key_mapping.el
@@ -3,8 +3,10 @@
 (global-set-key (kbd "M-.") 'gtags-find-rtag)
 
 (global-set-key (kbd "C-t") 'gtags-pop-stack)
+
 ;(global-set-key (kbd "M-g M-f") 'gtags-find-file)
 ;(global-set-key (kbd "M-g M-s") 'gtags-find-symbeol)
 ;(global-set-key (kbd "M-g M-u") 'gtags-update)
 
-
+;(global-set-key (kbd "C-M-g") 'goto-line)
+(define-key global-map "\C-c\C-g" 'goto-line)
